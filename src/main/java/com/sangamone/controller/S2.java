7dba21fc47332530c556e09b4461b4886c375d40
Ritesh kumar <riteshkumaryadav.sangamone@gmail.com>
2023-10-06 07:06:38 +0530

    completed bank

--- src/main/java/com/sangamone/controller/S2.java
+++ src/main/java/com/sangamone/controller/S2.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class S2 {
 	
 	@GetMapping("/result")
-	public List emi() {
+	public List<String> emi() {
 	List <String> list1 = new ArrayList<>();// name
-	list1.add("James");
+	list1.add("James"); // index 0
 	list1.add("Peter");
 	list1.add("Susan");
 	
 	List <Integer> list2 = new ArrayList<>();// principle
-	list2.add(10000);
+	list2.add(10000);//0
 	list2.add(8000);
 	list2.add(5000);
 	
-	List <Integer> list3 = new ArrayList<>();// time
-	list3.add(3);
+	List <Integer> list3 = new ArrayList<>();// time in year
+	list3.add(3);//
 	list3.add(2);
 	list3.add((int) 1.5);
 	
@@ -44,15 +44,23 @@ public class S2 {
 	
 	List <Integer> list7 = new ArrayList<>();// emi per month
 	for(int i=0; i<list2.size();i++ ) {
-	list7.add(list6.get(i)/list3.get(i)*12);
+		
+	list7.add(list6.get(i)/(list3.get(i)*12));
+//	System.out.println(list6.get(0));
+//	System.out.println(list3.get(i)*12);
+
+	
 	}
 	//Dear list1.get(0)
 	//Congratulations on taking a loan of $Principal from Silicon Valley Bank. You requested to pay an EMI of $EMI for the next $Months
-	List<String> list8= new ArrayList<>(); // message
-	list8.add("Dear "+ list1.get(0)+","+"\n Congratulations on taking a loan of "+list2.get(0)+" "
-			+ "from Silicon Valley Bank. You requested to pay of an EMI of "+ list7.get(0)+ " for the next month ");
-	
+	List<String> list8 = new ArrayList<>();
+	for(int i=0;i<list2.size();i++) {
+	list8.add("Dear " + list1.get(i));
+	list8.add(" Congratulations on taking a loan of " + list2.get(i) + " " +
+	        "from Silicon Valley Bank. You requested to pay an EMI of " + list7.get(i) + " for the next "+list3.get(i)*12+" month");
+	}
 	return list8;
+
 	
 	}
 }
